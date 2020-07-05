f8d750a81839973346b86a56a119b483873fc0c3
DarlanGabriel <darlan.acari@gmail.com>
2020-07-05 18:59:48 -0300

    Iniciando tela de Login

---
+++ client/src/telaLogin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function telaLogin() {
+  return (
+    <div className="Login">
+      <header className="Tela-Login">
+        <p>
+          Tela Login
+        </p>
+      </header>
+    </div>
+  );
+}
