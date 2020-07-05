f88d04513b717f9a85efdb64966d2a83eaf6955e
DarlanGabriel <darlan.acari@gmail.com>
2020-07-05 19:20:06 -0300

    Alterando minimamente tela de login

--- client/src/telaLogin.js
+++ client/src/telaLogin.js
@@ -6,6 +6,8 @@ function telaLogin() {
       <header className="Tela-Login">
         <p>
           Tela Login
+          <input type="text" name ="email" value="none"></input>
+          <input type="password" name="password" value="none"></input>
         </p>
       </header>
     </div>
