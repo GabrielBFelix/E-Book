eb066433eed7bb3a053e223dce490751393a07de
DarlanGabriel <darlan.acari@gmail.com>
2020-07-14 16:13:32 -0300

    Criando a tela de ver perfil

---
+++ client/src/pages/verPerfil/VerPerfil.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+function VerPerfil() {
+
+  return (
+    <div className="perfil">
+      <p>Perfil</p>
+      <div className="inputs">
+        <div>
+          <input type="text" name ="nome" 
+          value={(event)=>getNome(event.target.value)}></input>
+        </div>
+        <div>
+          <input type="text" name ="email" 
+          value={(event)=>getEmail(event.target.value)}></input>
+        </div>
+
+        <div>
+          <input type="password" name="password" 
+          value={(event)=>getPassword(event.target.value)}></input>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default VerPerfil;
