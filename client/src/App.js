b2884b9c881dfa601a736381aec5025d16760bc7
antoniofern <antoniofern.n@gmail.com>
2020-07-20 16:13:50 -0300

    Adicionando importação da api e alterando estrutura da landing page para classe

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Routes from "./routes";
-import Header from './components/Header';
+import Header from "./components/Header";
+import api from "./services/api";
 
 function App() {
   return (
