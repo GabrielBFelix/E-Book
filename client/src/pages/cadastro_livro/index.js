4e731dd3e8ed94d8ff65ac0c3bf18db1ea4a74d6
antoniofern <antoniofern.n@gmail.com>
2020-07-22 02:12:54 -0300

    Adicionando funcionalidade de cadastrar livro #65

--- client/src/pages/cadastro_livro/index.js
+++ client/src/pages/cadastro_livro/index.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Button, Form, FormGroup, Label, Input } from "reactstrap";
 
 import "./styles.css";
+import api from "../../services/api";
 
 const Livro = () => {
+
+  const [name, setName] = useState("");
+  const [author, setAuthor] = useState("");
+  const [genres, setGenre] = useState("");
+  const [publisher, setPublisher] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+
+  const saved_token = localStorage.getItem('token');
+
+  async function handleSubmit() {
+    const result = await api.post('/books', { name, author, genres, publisher, quantity, price, description }, { headers: { 'authorization' : `Bearer ${saved_token}` } });
+    console.log(result);
+  }
+
   return (
     <div className="forms">
       <Form className="book-form">
@@ -11,42 +28,46 @@ const Livro = () => {
         <FormGroup row>
           <Label sm={2}>Nome</Label>
           <Col sm={10}>
-            <Input type="text" placeholder="Nome do livro"></Input>
+            <Input type="text" placeholder="Nome do livro" value={name} onChange={(event)=>setName(event.target.value)}></Input>
           </Col>
         </FormGroup>
         <FormGroup row>
           <Label sm={2}>Autor</Label>
           <Col sm={10}>
-            <Input type="text" placeholder="Nome do autor" id></Input>
+            <Input type="text" placeholder="Nome do autor" value={author} onChange={(event)=>setAuthor(event.target.value)}></Input>
           </Col>
         </FormGroup>
         <FormGroup row>
           <Label sm={2}>Genêro</Label>
           <Col sm={10}>
-            <Input type="text" placeholder="Gênero"></Input>
+            <Input type="text" placeholder="Gênero" value={genres} onChange={(event)=>setGenre(event.target.value)}></Input>
           </Col>
         </FormGroup>
         <FormGroup row>
           <Label sm={2}>Editora</Label>
           <Col sm={10}>
-            <Input type="text" placeholder="Editora"></Input>
+            <Input type="text" placeholder="Editora" value={publisher} onChange={(event)=>setPublisher(event.target.value)}></Input>
           </Col>
         </FormGroup>
         <FormGroup row>
           <Label sm={2}>Quantidade</Label>
           <Col sm={10}>
-            <Input type="number" placeholder="Quantidade"></Input>
+            <Input type="number" placeholder="Quantidade" min="1" value={quantity} onChange={(event)=>setQuantity(event.target.value)}></Input>
           </Col>
         </FormGroup>
         <FormGroup row>
-          <Label sm={2}>Imagem do Livro</Label>
+          <Label sm={2}>Preço</Label>
           <Col sm={10}>
-            <Input type="url" placeholder="URL da imagem do livro"></Input>
+            <Input type="number" placeholder="Preço" pattern="^\d*(\.\d{0,2})?$" value={price} onChange={(event)=>setPrice(event.target.value)}></Input>
           </Col>
         </FormGroup>
+        <FormGroup>
+          <Label for="exampleText">Descrição</Label>
+          <Input type="textarea" placeholder="Descrição do livro..." value={description} onChange={(event)=>setDescription(event.target.value)}  />
+        </FormGroup>
         <div className="buttons">
-          <Button color="danger">Cancelar</Button>
-          <Button color="primary">Cadastrar</Button>
+          <Button color="danger">Voltar</Button>
+          <Button color="primary" onClick={handleSubmit}>Cadastrar</Button>
         </div>
       </Form>
     </div>
