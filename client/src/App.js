71cb063db22e387b45c5d5a7fe0646b3c5a10e9b
antoniofern <antoniofern.n@gmail.com>
2020-07-22 16:16:33 -0300

    Adicionando rota e a pagina base de ver perfil #64

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,51 @@
-import React, { useContext } from 'react';
-import Header from './components/Header';
-import api from './services/api';
+import React, { useContext } from "react";
+import Header from "./components/Header";
+import api from "./services/api";
 
+import { Switch, Route, Redirect } from "react-router-dom";
 
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { UserContext } from "./contexts/UserContext";
 
-import { UserContext } from './contexts/UserContext';
-
-import LoginPage from './pages/login/';
-import CadastroPage from './pages/cadastro';
-import Livro from './pages/cadastro_livro';
-import HomePage from './pages/landing_page';
-import DetailBook from './pages/detail_book/'
+import LoginPage from "./pages/login/";
+import CadastroPage from "./pages/cadastro";
+import Livro from "./pages/cadastro_livro";
+import HomePage from "./pages/landing_page";
+import DetailBook from "./pages/detail_book/";
+import VerPerfil from "./pages/verPerfil";
 
 function App() {
-
   const userContext = useContext(UserContext);
-
 
   return (
     <div className="App">
       <Header />
       <Switch>
-        <Route path='/books/:id' component={DetailBook}/>
-        
+        <Route path="/books/:id" component={DetailBook} />
+
         <Route exact path="/" component={HomePage} />
         <Route
           exact
           path="/login"
-          render={(...props) => (userContext.user ? <Redirect to="/" /> : <LoginPage {...props}></LoginPage>)}
+          render={(...props) =>
+            userContext.user ? (
+              <Redirect to="/" />
+            ) : (
+              <LoginPage {...props}></LoginPage>
+            )
+          }
         />
         <Route path="/cadastro" component={CadastroPage} />
         <Route
           path="/cadastro_livro"
-          render={(...props) => (userContext.user ? <Livro {...props} /> : <Redirect to="/" />)}
+          render={(...props) =>
+            userContext.user ? <Livro {...props} /> : <Redirect to="/" />
+          }
+        />
+        <Route
+          path="/user"
+          render={(...props) =>
+            userContext.user ? <VerPerfil {...props} /> : <Redirect to="/" />
+          }
         />
       </Switch>
     </div>
